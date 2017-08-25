3d485fe96b3d1dbd7dd3e79ae56f4a323e5535d1
Paul Yousefi <py16069@bris.ac.uk>
2017-08-25 17:46:12 +0100

    added download.supplementary.file function

--- R/download-series-files.R
+++ R/download-series-files.R
@@ -19,7 +19,7 @@ geograbr.download.series.files <- function(path=".", gpl = "GPL13534", gses) {
                       geo, "_series_matrix.txt.gz", sep="")
         urls <- c(urls, gsub("_series_", paste0("-",gpl,"_series_"), urls[1]))
         
-        cat(date(), geo, urls[1], "\n")
+        base::cat(base::date(), geo, urls[1], "\n")
 
         for (url in urls) {
             destination <- file.path(path, basename(url))
@@ -41,4 +41,39 @@ geograbr.download.series.files <- function(path=".", gpl = "GPL13534", gses) {
     })
 
     filenames
+}
+
+#' Download a GEO supplementary file
+#'
+#' Takes a GSE and a corresponding GEO supplementary file name
+#' and downloads that file to a specified directory
+#'
+#' @param path path for saving downloaded GEO series matrix files
+#' @param gse character of gse of interest  
+#' @param sup.file.name name of specific supplemental file from gse to downloads 
+#'
+#' @export
+geograbr.download.supplementary.file <- function(path=".", gse, sup.file.name) {
+    require(RCurl)
+        prefix <- substring(gse,1,5)
+        url <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/",
+                      prefix, "nnn/",
+                      gse, "/suppl/",
+                      sup.file.name, sep="")
+        
+        base::cat(base::date(), gse, url[1], "\n")
+
+        destination <- file.path(path, basename(url))
+        if (file.exists(destination)) return(destination)
+    
+        destination <- file.path(path, basename(url))
+        tryCatch(download.file(url, destfile=destination),
+                 error=function(e) {
+                     unlink(destination)
+                     print(e)
+                 })
+        Sys.sleep(3)
+        if (file.exists(destination)) 
+            return(destination)
+    NA
 }
