438ac49679b0ca41ce497ba0c6dbadc9dd7ad53d
Paul Yousefi <py16069@bris.ac.uk>
2019-01-22 21:50:01 +0000

    revised to automatically extract all characteristics fields to data frame

--- R/extract-characteristic.R
+++ R/extract-characteristic.R
@@ -1,17 +1,28 @@
+extract.characteristic <- function(ch) {
+    sub("([^:]+): (.*)", "\\2", ch)
+}
+
+extract.name <- function(ch) {
+    sub("([^:]+): (.*)", "\\1", ch)
+}
+
 #' Extract a characteristic field in GEO sample data
 #' 
 #' Pattern searches (greps) for a particular pattern in a data object with 
 #' sample
 #' 
-#' @param x GEO characteristc field, say as returned by 
-#' \code{geograbr.get.samples}
-#' @param pattern for grep search
+#' @param sample.frame a data.frame of descriptives for a GEO experiment (i.e GSE) 
+#' say as returned by \code{geograbr.get.samples}
+#' @param chr.field.str string that identifies the names of the characteristic
+#' columns to be extracted
 #' 
 #' @export
 #' 
-geograbr.extract.characteristic <- function(x, pattern) {
-    idx <- grep(pattern, x, ignore.case=T)
-    ret <- rep("", length(x))
-    ret[idx] <- sub(paste0(".*((", pattern, ")[^:]*: ([^;]+)).*"), "\\3", x[idx], ignore.case=T)
-    ret
+geograbr.extract.characteristics <- function(sample.frame, 
+						chr.field.str = "characteristics") {
+	chrs <- grep(chr.field.str, names(sample.frame))
+	ret <- data.frame(lapply(sample.frame[,chrs], extract.characteristic), 
+				stringsAsFactors = F)
+	names(ret) <- extract.name(sample.frame[1,chrs])
+	ret
 }
