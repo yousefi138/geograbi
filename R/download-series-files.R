97f97a6316e7bb33e8b00158d7c326071c1ebba6
Matthew Suderman <matthewsuderman@hotmail.com>
2019-01-28 15:28:08 +0000

    fixes geograbr.read.gse.matrix

--- R/download-series-files.R
+++ R/download-series-files.R
@@ -8,16 +8,11 @@
 #' @param gses vector of gses to download from GEO 
 #' @param gpls vector of platform gpl codes with same length as \code{gses}
 #'
-#' @export
-#' 
+#' @export 
 geograbr.download.series.files <- function(path=".", gses, gpls = NULL) {
     require(RCurl)
     filenames <- sapply(seq_along(gses), function(gse) {
-        prefix <- substring(gses[gse],1,5)
-        urls <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/",
-                      prefix, "nnn/",
-                      gses[gse], "/matrix/",
-                      gses[gse], "_series_matrix.txt.gz", sep="")
+        urls <- get.gse.matrix.url(gses[gse])
         if(!is.null(gpls)){           
           urls <- c(urls, gsub("_series_", paste0("-",gpls[gse],"_series_"), urls[1]))
         }
@@ -53,34 +48,63 @@ geograbr.download.series.files <- function(path=".", gses, gpls = NULL) {
 #'
 #' @param path path for saving downloaded GEO series matrix files
 #' @param gse character of gse of interest  
-#' @param sup.file.name name of specific supplemental file from gse to downloads 
+#' @param filename name of specific supplemental file from gse to downloads 
 #' @param url optional manual url supply for non-ftp based file downloads
 #'
 #' @export
-geograbr.download.supplementary.file <- function(path=".", gse, sup.file.name=NULL, url=NULL) {
+geograbr.download.supplementary.file <- function(path=".", gse=NULL, filename=NULL, url=NULL) {
     require(RCurl)
-    if (is.null(url)){
-        prefix <- substring(gse,1,5)
-        url <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/",
-                      prefix, "nnn/",
-                      gse, "/suppl/",
-                      sup.file.name, sep="")
-        destination <- file.path(path, basename(url))
-      } else {
-        destination <- file.path(path, sup.file.name)
-      }
-        base::cat(base::date(), gse, url[1], "\n")
-
-        if (file.exists(destination)) return(destination)
+    if (is.null(url)) {
+        stopifnot(!is.null(gse) && !is.null(filename))
+        url <- file.path(get.gse.supp.url(gse), filename) 
+    }        
+    destination <- file.path(path, basename(url))
     
-        tryCatch(download.file(url, destfile=destination),
-                 error=function(e) {
-                     unlink(destination)
-                     print(e)
-                 })
-        Sys.sleep(3)
-        if (file.exists(destination)) 
-            return(destination)
+    base::cat(base::date(), url[1], "\n")
+    if (file.exists(destination)) 
+        return(destination)
+    tryCatch(download.file(url, destfile = destination), error = function(e) {
+        unlink(destination)
+        print(e)
+    })
+    Sys.sleep(3)
+    if (file.exists(destination)) 
+        return(destination)
     NA
 }
 
+
+#' @param x A character string.
+#' @return \code{TRUE} if \code{x} is a valid GSE identifier,
+#' otherwise \code{FALSE}.
+is.gse <- function(x) {
+    grepl("^gse[0-9]+$", x, ignore.case = T)
+}
+
+#' @param x GSE identifier
+#' @return The identifier with the final 3 digits
+#' replaced with 'nnn'. If there are fewer than
+#' 3 digits, then all digits are replaced.
+gse.prefix <- function(x) {
+    x <- toupper(x)
+    if (nchar(x) < 6) "GSEnnn"
+    else sub("[0-9]{3}$", "nnn",x)
+}
+
+
+#' @param x GSE identifier
+#' @return the url of the series matrix file
+get.gse.matrix.url <- function(x) {
+    prefix <- gse.prefix(x)
+    paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/", 
+          prefix, "/", x, "/matrix/", x, "_series_matrix.txt.gz", 
+          sep = "")
+}
+
+#' @param x GSE identifier
+#' @return the url of the supplementary folder for the GSE
+get.gse.supp.url <- function(x) {
+    prefix <- gse.prefix(x)
+    paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/", 
+          prefix, "/", x, "/suppl/", sep = "")
+}
--- R/get.R
+++ R/get.R
@@ -1,4 +1,5 @@
-#' Get GEO sample information 
+#' Get GEO sample information
+#' @export
 geograbr.get.samples <- function(filename, FUN=geograbr.read.gse.matrix) {
     gse <- tolower(names(filename))
     cat(date(), "reading", gse,"\n")
@@ -13,8 +14,7 @@ geograbr.get.samples <- function(filename, FUN=geograbr.read.gse.matrix) {
 #' @param filename 
 #' 
 #' @export
-#' 
-geograbr.get.data <- function(filename, path=NULL, geo.sites=NULL,  
+geograbr.get.data <- function(filename, path=NULL, 
     FUN=geograbr.read.gse.matrix) {
     gse <- tolower(names(filename))
 
@@ -35,89 +35,245 @@ geograbr.get.data <- function(filename, path=NULL, geo.sites=NULL,
 
 is.url <- function(x) "url" %in% class(tryCatch(url(x), error=function(e) e))
 
-#' Read GEO gse series matrix
-#' 
-#' Interface for reading a gse series matrix file as downloaded by 
-#' \code{geograbr.download.series.files}
-#' 
-geograbr.read.gse.matrix <- function(filename, data=TRUE) {
-    # parse filename 
-    if(grepl("^gse*", filename, ignore.case = T)) {
-        prefix <- sub("([0-9]{3}$)", "nnn", filename)        
-        filename <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series/",
-                      prefix, "/",
-                      filename, "/matrix/",
-                      filename, "_series_matrix.txt.gz", sep="")
+
+
+#' Read lines from a connection until a pattern is matched
+#'
+#' @param pattern Character pattern to search for using \code{\link{grep}()}.
+#' @param con A connection object or filename.
+#' @param n The starting number of lines to read.  Each time the pattern
+#' is not identified, the number of lines is doubled.
+#' @param ... Additional arguments to \code{\link{readLines}()}.
+#' @return The initial lines read from the connection with at least
+#' one matching the pattern or all lines available from the connection
+#' if the pattern was not matched.
+read.until <- function(pattern, con, n=100, ...) {
+    if (is.character(con)) {
+        con <- file(con, "r")
+        on.exit(close(con))
+    }
+        
+    lines <- c()
+    finished <- F
+    while (!finished) {
+        new <- readLines(con=con, n=n, ...)
+        lines <- c(lines, new)
+        finished <- (length(grep(pattern, new)) > 0
+                     || length(new) < n)
+        n <- 2*n
+    }
+    lines
+}
+
+#' Load a GSE matrix file from GEO
+#'
+#' @param filename A character string that is a GSE identifier,
+#' a url for GSE matrix file online, or a filename for a local
+#' GSE matrix file.
+#' @param data If \code{TRUE}, then load the data matrix,
+#' otherwise load only the sample information.
+#' @return A list with two elements:
+#' \itemize{
+#' \item{"samples"}{The sample data frame with one row per sample.}
+#' \item{"data"}{The data matrix with one row per feature and one per sample.}
+#' }
+#' @export
+geograbr.read.gse.matrix <- function (filename, data = TRUE) {
+    ## if filename is a GSE number, then obtain the url
+    if (is.gse(filename))
+        filename <- get.gse.matrix.url(filename)
+
+    ## if filename is a url and we want the data matrix
+    ## then it's most efficient to download the file
+    if (is.url(filename) & data) {
+        file.url <- filename
+        filename <- basename(file.url)
+        download.file(file.url, destfile=filename)
+        on.exit(unlink(filename))
     }
 
-    if (is.url(filename)) {
-        filename <- gzcon(url(filename))
-    } 
-
-    i <-100; dat <-NULL;
-    while (sum(sign(grepl("!series_matrix_table_begin", dat)))<1) {
-        dat <- c(dat, readLines(filename, n=i))
-       i = i*2
-    }
-
-    nseries <- sum(grepl("^!Series_", dat))
-    nsamples <- sum(grepl("^!Sample_", dat))
-    ndata <- length(dat) - match("!series_matrix_table_begin", dat) - 2
-    con <- textConnection(dat)
-    header <- read.table(con, sep="\t", header=F, nrows=nseries, 
-        stringsAsFactors=F)
-    samples <- read.table(con, sep="\t", header=F, nrows=nsamples, 
-        stringsAsFactors=F)
-    closeAllConnections()    
-    samples <- t(samples)
-    colnames(samples) <- samples[1,]
-    colnames(samples) <- sub("!Sample_", "", colnames(samples))
-    samples <- data.frame(samples[-1,], stringsAsFactors=F)
-    rm(dat)
-    gc()
-
-    if(data==FALSE) return(samples)
+    ## open a conection the file/url
+    if (is.url(filename))
+        con <- gzcon(url(filename))
+    else
+        con <- file(filename, "r")
     
-    con <- file(filename, "r")
-    readLines(con,1)
-    data <- read.table(con, sep="\t", header=TRUE, quote="\"", dec=".", 
-        fill=TRUE,
-        na.strings = c("NA", "null", "NULL", "Null"), comment.char = "")
+    ## read until the data matrix starts
+    lines <- read.until("!series_matrix_table_begin", con)
+
     close(con)
-    if (ndata == 0)
-        data <- data[-(1:nrow(data)),]
+
+    ## number of lines about the series
+    nseries <- sum(grepl("^!Series_", lines))
+    ## number of lines about the samples
+    nsamples <- sum(grepl("^!Sample_", lines))
+
+    ## position in the file where the data matrix starts
+    matrix.start <- which(lines == "!series_matrix_table_begin") + 1
+
+    ## parse series and sample information
+    txt <- textConnection(lines)    
+    header <- read.table(txt, sep = "\t", header = F, nrows = nseries, 
+                         stringsAsFactors = F)    
+    samples <- read.table(txt, sep = "\t", header = F, nrows = nsamples, 
+                          stringsAsFactors = F)
+    close(txt)
+
+    ## transpose so we have 1 row per sample
+    samples <- t(samples)
+
+    ## the first row gives the column names
+    colnames(samples) <- sub("!Sample_", "", samples[1,])
+    samples <- data.frame(samples[-1, ], stringsAsFactors = F)
+    rownames(samples) <- samples$geo_accession
+
+    ## if just sample information requested, then return
+    if (data == FALSE) 
+        return(samples)
+
+    ## otherwise, reopen the file
+    con <- file(filename, "r")
+
+    ## read to the beginning of the matrix
+    readLines(con, matrix.start-1) 
+
+    ## read the matrix
+    data <- read.table(con, sep = "\t", header = TRUE, quote = "\"", 
+                       dec = ".", fill = TRUE,
+                       na.strings = c("NA", "null", "NULL", "Null"),
+                       comment.char = "")
+    close(con)
+
+    ## if the data matrix is empty (GEO sometimes does this!),
+    ## then the matrix will contain one row of NAs, remove them.
+    if (nrow(data) <= 1)
+        data <- data[-(1:nrow(data)),,drop=F]
     
+    ## the first column identifies the features (rows) of the matrix
     if (nrow(data) > 0) 
-        rownames(data) <- data[,1]
-    data <- as.matrix(data[,-1])
-    
+        rownames(data) <- data[, 1]
+    data <- as.matrix(data[, -1])
+
     rownames(samples) <- colnames(data)
-    colnames(data) <- samples$geo_accession
+    colnames(data) <- as.character(samples$geo_accession)
 
-    if (length(intersect(colnames(data), samples$geo_accession)) == 0)
-        return(0)
+    ## ... isn't this equivalent to ncol(data) == 0
+    ## because of the line immediately before?
+    #if (length(intersect(colnames(data), samples$geo_accession)) == 0) 
+    #    return(0)
 
-    if (is.data.frame(data))
-        data <- as.matrix(data)
-    
-    if (any(data < -0.5 | data > 1.5, na.rm=T))
+    if (any(data < -0.5 | data > 1.5, na.rm = T)) 
         data <- ilogit2(data)
 
-    stopifnot(length(rownames(data)) == nrow(data))
-    stopifnot(length(colnames(data)) == ncol(data))
-    if(!is.null(geo.sites)) stopifnot(length(intersect(rownames(data), 
-        geo.sites)) > 100000)
-    
-    i <- match(colnames(data), as.character(samples$geo_accession))
-    if (any(is.na(i))) {
-        data <- data[,which(!is.na(i)), drop=F]
-    }
-    
-    if(!is.null(geo.sites)) data <- data[match(geo.sites, rownames(data)),,
-        drop=F]
+    ## ... isn't this true by definition?
+    #stopifnot(length(rownames(data)) == nrow(data))
+    #stopifnot(length(colnames(data)) == ncol(data))
 
-   list(data=data, samples=samples)
+    ## ... above colnames(data) is assigned to samples$geo_accession
+    ## so not sure what this is doing.
+    #i <- match(colnames(data), as.character(samples$geo_accession))
+    #if (any(is.na(i))) {
+    #    data <- data[, which(!is.na(i)), drop = F]
+    #}
+
+    ## ... annoying if this fails, user would want to know why
+    ## perhaps better to check outside this function.
+    #if (!is.null(geo.sites)) 
+    #    stopifnot(length(intersect(rownames(data), geo.sites)) > 1e+05)
+    #if (!is.null(geo.sites)) 
+    #    data <- data[match(geo.sites, rownames(data)), , drop = F]
+    
+    list(data = data, samples = samples)
 }
 
 
+#' Read a very large table
+#' 
+#' \code{\link{read.table}()} is extremely memory inefficient and slow.
+#' This function somewhat resolves this by applying \code{\link{read.table}()}
+#' multiple times to partitions of the table.
+#'
+#' @param file A character connection or filename.
+#' @param header A logical indicating if the file contains a header
+#' (default: \code{FALSE}).
+#' @param chunk.size Size of partitions to read in bytes
+#' (default: 21234).
+#' @param verbose A logical indicating whether or not to print
+#' status updates (default: \code{FALSE}).
+#' @param debug A logical indicating whether or not to run
+#' \code{\link{browser}()} after attempting to read the file partitions.
+#' @param ... Additional arguments passed to \code{\link{read.table}()}.
+#' @return A data frame as if \code{\link{read.table}()} had been used.
+#' 
+#' @export
+geograbr.read.table <- function(file, header=F, chunk.size=21234, verbose=F, debug=F, ...) {
+    if (is.character(file)) {
+        file <- file(file, "r")
+        on.exit(close(file))
+    }
+    x <- list()
+    is.more <- T
+    while (is.more) {
+        if (verbose)
+            cat(date(), "reading chunk", length(x), "\n")
+        tryCatch({
+            x[[length(x) + 1]] <- read.table(file, header=header && length(x) == 0,
+                                             nrows=chunk.size, ...)
+        }, error=function(e) {
+            is.more <<- F
+            if (e$message != "no lines available in input") {
+                signalCondition(e)
+            }              
+        })
+    }
+    if (debug)
+        browser()
+    if (length(x) > 1)
+        for (i in 2:length(x)) colnames(x[[i]]) <- colnames(x[[1]])
+    do.call(rbind, x)
+}
 
+
+#' Read a very large csv file
+#'
+#' This function is a wrapper for \code{\link{geograbr.read.table}()}
+#' for reading csv files.
+#'
+#' @examples
+#' \donttest{
+#' ## download a DNA methylation dataset
+#' dataset <- geograbr.read.gse.matrix("GSE116339")
+#' ## dataset$data is empty because sometimes GEO
+#' ## stores the genomic data in a supplementary file
+#' ## instead of in the series matrix file
+#'
+#' ## download the supplementary file
+#' filename <- geograbr.download.supplementary.file(gse="GSE116339",
+#'                     filename="GSE116339_Processed_Matrix.csv.gz")
+#' ## 15 minutes
+#'
+#' ## load the file
+#' dataset$data <- geograbr.read.csv(filename)
+#' ## 35 minutes
+#'
+#' ## make the first column the rownames (features)
+#' ## and convert to a matrix
+#' sites <- as.character(dataset$data[,1])
+#' dataset$data <- as.matrix(dataset$data[,-1])
+#' rownames(dataset$data) <- sites
+#'
+#' ## reorder the samples according to data matrix columns
+#' colnames(dataset$data) <- sub("X", "", colnames(dataset$data))
+#' idx <- match(colnames(dataset$data), dataset$samples$title)
+#' dataset$samples <- dataset$samples[idx,]
+#' }
+#' 
+#' @export
+geograbr.read.csv <- function(file, header = TRUE, sep = ",", quote = "\"", dec = ".",
+                              fill = TRUE, comment.char = "",
+                              verbose=F, debug=F, chunk.size=21234, ...) {
+    geograbr.read.table(file = file, header = header, sep = sep, quote = quote,
+                        dec = dec, fill = fill, comment.char = comment.char,
+                        chunk.size=chunk.size,
+                        verbose=verbose, debug=debug,...)
+}
